2b2cd3740946450cc9112b78e43f81c6c60b2257
Uri Shamay <cmpxchg16@gmail.com>
2022-09-19 12:40:39 +0300

    fix azurerm_storage_account enable_https_traffic_only

--- src/infra/mssql/mssql.tf
+++ src/infra/mssql/mssql.tf
@@ -5,11 +5,12 @@ resource "azurerm_resource_group" "example" {
 
 
 resource "azurerm_storage_account" "example" {
-  name                     = "accteststorageaccount"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
+  name                      = "accteststorageaccount"
+  resource_group_name       = azurerm_resource_group.example.name
+  location                  = azurerm_resource_group.example.location
+  account_tier              = "Standard"
+  account_replication_type  = "GRS"
+  enable_https_traffic_only = true
   blob_properties {
     delete_retention_policy {
       days = 30
